c22c9074edcff9b5afda46af76d2a0d68a41ce3e
Telnesito <carlosj_1703@hotmail.com>
2023-06-20 22:12:31 -0400

    fix-bug

--- nutri-ne/src/main.jsx
+++ nutri-ne/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/routes'
 import '../index.css'
-import Theme from './style/theme'
+import Theme from './style/Theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
